b14537a7114b8e4f12991e59e677853c2c085262
Jennifer Moore <jenniferplusplus@gmail.com>
2019-05-25 23:45:44 -0500

    Added api endpoint that returns json for testing against

--- SquadGoals.Api/ApiCall.cs
+++ SquadGoals.Api/ApiCall.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -10,7 +11,7 @@ namespace SquadGoals.Api
     public static class ApiCall
     {
         [FunctionName("Status")]
-        public static IActionResult Run(
+        public static IActionResult GetStatus(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = "status")]
             HttpRequest req, ILogger log)
         {
@@ -18,5 +19,34 @@ namespace SquadGoals.Api
             
             return new OkObjectResult("Connected to API, status ok");
         }
+        
+        [FunctionName("User")]
+        public static IActionResult GetUser(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "user")]
+            HttpRequest req, ILogger log)
+        {
+            log.LogInformation("Status - HTTP trigger");
+            
+            var pronouns = new Dictionary<string, string>();
+            pronouns.Add("subject", "she");
+            pronouns.Add("object", "her");
+            pronouns.Add("possesive", "hers");
+
+            var user = new User()
+            {
+                displayName = "Static Test User",
+                id = 10001,
+                pronouns = pronouns
+            };
+            
+            return new JsonResult(user);
+        }
+    }
+
+    public struct User
+    {
+        public int id;
+        public string displayName;
+        public Dictionary<string, string> pronouns;
     }
 }
